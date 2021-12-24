f156ddce20a830d8fcde163a5537c704a5bca51e
Akedos <mukashevakedos02@gmail.com>
2021-12-24 17:58:19 +0600

    прпрра

--- app/src/main/java/com/geektech/my1dz39/MainActivity.java
+++ app/src/main/java/com/geektech/my1dz39/MainActivity.java
@@ -2,22 +2,51 @@ package com.geektech.my1dz39;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
-private ImageView imageView;
+
+    private ImageView imageView;
+    private EditText editText,editText2;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id._image);
+        editText = findViewById(R.id.ed_username);
+        editText2 = findViewById(R.id.ed_password);
+        button = findViewById(R.id.button_send);
         Glide.with(this).load("https://encrypted-tbn0.gstatic." +
                 "com/images?q=tbn:ANd9GcRIBwO3OL2BZuyaVukHIM0nzBry_awjPRuNUQ&usqp=CAU").into(imageView);
-        Log.d("Ololo", "onCreate: Rojdaemsay");
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editText.getText().toString().length() < 6 ){
+                    Toast.makeText(MainActivity.this, "Имя пользователя не может быть меньше 6 символов", Toast.LENGTH_SHORT).show();
+                }else if (editText2.getText().toString().length() < 6){
+                    Toast.makeText(MainActivity.this, "Пароль должен состоят из 6 символов", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                    intent.putExtra("key",editText.getText().toString());
+                    intent.putExtra("key2",editText2.getText().toString());
+                    startActivity(intent);
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/geektech/my1dz39/SecondActivity.java
+++ app/src/main/java/com/geektech/my1dz39/SecondActivity.java
@@ -1,14 +1,79 @@
 package com.geektech.my1dz39;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private EditText editText,editText2;
+    private TextView textView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.editPhoto);
+        editText = findViewById(R.id.editUsername);
+        editText2 = findViewById(R.id.editPassword);
+
+       String username = getIntent().getStringExtra("key");
+       String password = getIntent().getStringExtra("key2");
+       editText.setText(username);
+       editText2.setText(password);
+
+       ActivityResultLauncher<String>resultLauncher = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+                   @Override
+                   public void onActivityResult(Uri result) {
+                       Glide.with(SecondActivity.this).load(result).circleCrop().into(imageView);
+                   }
+               }
+       );
+
+        ActivityResultLauncher<Intent> resultLauncherForCamera = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                Glide.with(SecondActivity.this).load(result).circleCrop().into(imageView);
+            }
+        });
+
+       textView.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+              AlertDialog.Builder alert =new AlertDialog.Builder(SecondActivity.this);
+              alert.setPositiveButton("Выбрать из галереи", new DialogInterface.OnClickListener() {
+                  @Override
+                  public void onClick(DialogInterface dialog, int which) {
+                      resultLauncher.launch("image/*");
+                  }
+              }).setNegativeButton("Выбрать камеру", new DialogInterface.OnClickListener() {
+                  @Override
+                  public void onClick(DialogInterface dialog, int which) {
+                      resultLauncherForCamera.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
+
+                  }
+              }).show();
+           }
+       });
+
     }
 }
